780a3ed3d0ca1c21ec4cbc929b75b81490a5c178
renelps <renanlps616@gmail.com>
2025-10-07 09:35:13 -0300

    refactor(page:VehicleInfo): Convert page to Server Component

--- src/app/private/vehicles/vehicle_info/[id]/page.tsx
+++ src/app/private/vehicles/vehicle_info/[id]/page.tsx
@@ -1,13 +1,16 @@
 
-import { use } from 'react';
 import { getVehicleDetails } from '../get_vehicles/get_vehicles';
 import { VehicleDetails } from '@/lib/types/vehicleDetails';
 import { VehicleContent } from '../_components/vehicle_content';
 
-export default function VehicleInfo({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function VehicleInfo({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   
-  const vehicle: VehicleDetails =  use(getVehicleDetails(id));
+  const vehicle: VehicleDetails = await getVehicleDetails(id);
 
 
   return <VehicleContent data={vehicle} />
