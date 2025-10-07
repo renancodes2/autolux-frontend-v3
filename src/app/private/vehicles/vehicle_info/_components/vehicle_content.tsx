ab4a1eca17a217c2ed3221f374093f52c88b174f
renelps <renanlps616@gmail.com>
2025-10-07 09:34:35 -0300

    feat(component:VehicleContent): Implement initial layout for vehicle details

--- src/app/private/vehicles/vehicle_info/_components/vehicle_content.tsx
+++ src/app/private/vehicles/vehicle_info/_components/vehicle_content.tsx
@@ -1,13 +1,28 @@
 import { VehicleDetails } from "@/lib/types/vehicleDetails"
+import Image from "next/image";
 
 interface VehicleContentProps {
   data: VehicleDetails;
 }
 
 export function VehicleContent({ data }: VehicleContentProps){
+  console.log(data?.imageUrls[0])
   return (
-    <div>
-      <h2>{data?.name}</h2>
+    <div className="flex items-center justify-center">
+      <section className="flex flex-col md:flex-row mt-15 gap-5">
+        <div className="relative w-[600px] h-96">
+            <Image
+              src={data.imageUrls[0]}
+              alt="logo"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-md shadow-md w-full"
+            />
+        </div>
+        <div>
+          <h2>Ford Ranger</h2>
+        </div>
+      </section>
     </div>
   )
 }
