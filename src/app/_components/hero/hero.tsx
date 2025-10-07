21ad6bb70de66176b02948836b67c1e890218714
renelps <renanlps616@gmail.com>
2025-10-07 09:33:05 -0300

    refactor(component:Hero): Adjust responsiveness and background image

--- src/app/_components/hero/hero.tsx
+++ src/app/_components/hero/hero.tsx
@@ -3,13 +3,13 @@
 export function Hero(){
   return (
     <section 
-      className="w-full h-[600px] flex items-center bg-gray-100 flex-col bg-cover bg-center relative"
-      style={{ backgroundImage: "url('/hero1.jpg')" }}
+      className="w-full h-[400px] md:h-[600px] flex items-center bg-gray-100 flex-col bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/logo2.jpg')" }}
     > 
 
     <div className="absolute inset-0 bg-black opacity-40"></div>
 
-      <h2 className="mt-20 text-5xl z-10 text-white font-bold">Auto Lux</h2>
+      <h2 className="mt-10 md:mt-20 text-5xl z-10 text-white font-bold">Auto Lux</h2>
       <h2 className="text-4xl font-bold mt-5 mb-20 text-white z-10">Qualidade e confiança sobre rodas</h2>
       <div className="shadow-md max-w-5xl w-full max-h-60 h-full rounded-md">
         <h2 className="text-center text-lg font-bold mt-4">Emcontre seu carros</h2>
